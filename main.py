bce2fed498b174a68084fa8c44b83c8209f350c4
Vincenzo D'Amore <v.damore@gmail.com>
2023-01-29 21:25:36 +0100

    first import

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from tgtg import TgtgClient, TgtgAPIError, TgtgLoginError, TgtgPollingError
 import asyncio 
-import pprint
-import time
 import hashlib
 import json
 import os
-from user_data import UserData
-from constants import CREDENTIALS_FNAME, USERS, OFFERS_HASH_FNAME
+from user_data import UserData, Offer
+from constants import CREDENTIALS_FNAME, USERS, OFFERS_HASH_FNAME, TELEGRAM_TOKEN
+from urllib.parse import quote
+
+from constants import TELEGRAM_TOKEN
+from telegram import Bot
 
 loop = asyncio.get_event_loop()
 
@@ -21,26 +23,28 @@ def get_offers(client: TgtgClient, user: UserData):
     )
     for item in items:
       if item['items_available'] > 0:
-        offers.append(f"{item['display_name']} {item['items_available']}")
+        offer = Offer(item['item']['item_id'], item['display_name'], item['items_available'])
+        offers.append(offer)
   return offers
 
-def user_has_newer_offers(offers, user: UserData):
-  hash_offers = ''
+def user_has_newer_offers(offers: list, user: UserData):
+  has_offers=False
   for offer in offers:
-    hash_offers += str(hashlib.md5(offer.encode()).hexdigest())
-  hash_fname = OFFERS_HASH_FNAME % user.email
-  if os.path.isfile(hash_fname):
-    with open(hash_fname, 'r') as f:
-      old_hash_offers = json.load(f)
-  else:
-    old_hash_offers = ''
-  if old_hash_offers != hash_offers:
-    with open(hash_fname, 'w') as f:
-      f.write(json.dumps(hash_offers))
-    return True
-  return False
+    hash_offer = str(hashlib.md5(offer.description.encode()).hexdigest())
+    hash_fname = OFFERS_HASH_FNAME % (user.email, hash_offer)
+    if os.path.isfile(hash_fname):
+      with open(hash_fname, 'r') as f:
+        old_hash_offers = json.load(f)
+    else:
+      old_hash_offers = ''
+    if old_hash_offers != hash_offer:
+      with open(hash_fname, 'w') as f:
+        f.write(json.dumps(hash_offer))
+        offer.is_new=True
+      has_offers=True
+  return has_offers
 
-def main():
+async def main():
   for user in USERS:
     credentials_fname = CREDENTIALS_FNAME % user.email
     if os.path.isfile(credentials_fname):
@@ -52,7 +56,12 @@ def main():
         user.loggedin=True
         offers = get_offers(client=client, user=user)
         if user_has_newer_offers(offers=offers, user=user):
-          print(offers)
+          for offer in offers:
+            if offer.is_new:
+              msg=offer.description
+              print(offer.description)
+              bot = Bot(TELEGRAM_TOKEN)
+              await bot.send_message(chat_id=user.chat_id, text=msg)
       except TgtgAPIError as e:
         os.remove(credentials_fname)
       except TgtgLoginError as e:
@@ -63,5 +72,4 @@ def main():
     # print(items)
 
 if __name__ ==  '__main__':
-    # loop.run_until_complete(main())
-    main()
+    loop.run_until_complete(main())
--- user_data.py
+++ user_data.py
@@ -1,7 +1,15 @@
 class UserData:
-  def __init__(self, email, lat, lon, radius):
+  def __init__(self, email, chat_id, lat, lon, radius):
     self.loggedin = False
     self.email = email
+    self.chat_id = chat_id
     self.lat = lat
     self.lon = lon
     self.radius = radius
+
+class Offer:
+  def __init__(self, offer_id, description, availability):
+    self.offer_id = offer_id
+    self.description = description
+    self.availability = availability
+    self.is_new = False
